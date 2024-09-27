5be66021f398315181e7778286cf5f0b0c83c4c2
hodu2 <akdma7526@gmail.com>
2024-09-27 15:37:35 +0900

    hi

--- src/main/java/com/kh/cart1/FrontController.java
+++ src/main/java/com/kh/cart1/FrontController.java
@@ -21,7 +21,7 @@ public class FrontController extends HttpServlet {
         String action = request.getParameter("action");
         Action actionInstance = null;
         
-        System.out.println("ddd");
+        System.out.println("dddddddd");
         
         if ("viewCart".equals(action)) {
             actionInstance = new ViewCartAction();
